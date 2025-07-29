321c3e02a8b5afd904b8eeb25f7c3c54b487d310
Aron Schüler <aron.schueler@ambient.digital>
2025-07-29 11:54:52 +0200

    fix: adjust base path in Astro configuration for meetup landing pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,17 @@ import { defineConfig } from "astro/config";
 const MEETUP_ID = process.env.MEETUP_ID || "cologne-js"; // default to cologne-js
 
 const getBase = () => {
-  return `/${MEETUP_ID}/`;
+  return `/meetup-landing-pages/${MEETUP_ID}/`;
 };
 
 // Set the site URL
 const getSite = () => {
-  return process.env.GITHUB_PAGES_URL ||
-    "https://ambient-innovation.github.io/meetup-landing-pages/" +
-      `${MEETUP_ID}/`;
+  return (
+    (process.env.GITHUB_PAGES_URL ||
+      "https://ambient-innovation.github.io/meetup-landing-pages/") +
+    MEETUP_ID +
+    "/"
+  );
 };
 
 // https://astro.build/config
